2cb2d71899b0b08aacfde0ffa79f78fee3898d64
SPIRT5 <108623559+SPIRT5@users.noreply.github.com>
2025-03-16 19:46:14 +0300

    Delete SimpleLogger.java

--- SimpleLogger.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDateTime;
-
-class SimpleLogger implements Logger {
-    @Override
-    public void log(String msg) {
-        System.out.println("[" + LocalDateTime.now() + "] " + msg);
-    }
-}
